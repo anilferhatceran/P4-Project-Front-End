7cb5b3eccba6633b26ef9032e68517f87a5fa387
Anil Ceran <anil@pc-tec.local>
2021-05-18 07:49:47 +0200

    Updated Password Generator

--- testing-app/src/app/Components/Password-gen/Password-gen.component.ts
+++ testing-app/src/app/Components/Password-gen/Password-gen.component.ts
@@ -7,49 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Password-gen.component.css']
 })
 export class PasswordGenComponent implements OnInit {
-  passGenForm:any;
-  passGeneratorTestForm: any; // <----- Also from PASSWORD GENERATOR TEST
-  ranNumbers:number = 0;
-  ranWordsLowcase:string = "";
-  ranWordsUppercase:string = "";
 
   constructor() { }
+  passGenForm:any;
+  passLengthForm: any;
+  displayedPass: any;
 
   ngOnInit() {
     this.passGenForm = new FormGroup({
-      passWord: new FormControl(''),
-    
-    });
-
-
-    // PASSWORD GENERATOR TESTING FROM HERE AND DOWN.
-    this.passGeneratorTestForm = new FormGroup({
-      row_password: new FormControl(),
+      password: new FormControl(),
+      passLength: new FormControl(),
     });
     }
+    randomPassword() {
 
-    randomPassword(length:any) {
     var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
     var pass = "";
-    for (var x = 0; x < length; x++) {
+    for (var x = 0; x < this.passGenForm.value.passLength; x++) {
         var i = Math.floor(Math.random() * chars.length);
         pass += chars.charAt(i);
     }
-    return pass;
-}
-generate(form:FormGroup) {
-    console.log(this.randomPassword(50))  // Den bliver registret i loggen i F12 konsol(husk at trykke på knappen når du er i konsol), men den bliver ikke output i selve tekstboksen og ved ikke hvorfor
-    var value = this.randomPassword(50)
-    this.passGeneratorTestForm.row_password = this.randomPassword(form.value.length);
-}
 
-// generate(form:FormGroup) {
-//   console.log(form.value.row_password);
-//     form.value.row_password = this.randomPassword(form.value.length);
-// }
-// generate(form:FormGroup) {
-//   var value = this.randomPassword(50)
-//    this.passGeneratorTestForm.row_password = value;
-
-// }
+    this.displayedPass = pass;
+  }
 }
