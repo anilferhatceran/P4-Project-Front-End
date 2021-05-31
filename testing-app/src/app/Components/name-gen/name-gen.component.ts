3c928cc25117172ea667fe11c4913621fb03f435
Anil Ceran <anil@pc-tec.local>
2021-05-31 12:16:07 +0200

    name gen updated

--- testing-app/src/app/Components/name-gen/name-gen.component.ts
+++ testing-app/src/app/Components/name-gen/name-gen.component.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { NamesGenerated } from './../../model/Names';
 import { HttpService } from 'src/app/service/http.service';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -12,14 +13,18 @@ import { Component, OnInit } from '@angular/core';
 export class NameGenComponent implements OnInit {
   nameGenForm:any;
   ranName: string = "";
-  generatedMaleNames: NamesGenerated = {nameGenID:0, maleNames: '',femaleNames:''};
+  generatedMaleNames: NamesGenerated;
   generatedFemaleNames: NamesGenerated;
   maleNameChoice:boolean = true;
   femaleNameChoice:boolean = true;
   testName: string = '';
-  splitOne: string[];
+  testNameTest: number;
+  namesArray: string[];
   maleName: any;
   femaleName: any;
+  newStr: any;
+  newStr2: any;
+  saveNameForm: FormGroup;
 
 
 
@@ -30,10 +35,16 @@ export class NameGenComponent implements OnInit {
       femaleNameChoice: new FormControl(),
       maleNameChoice: new FormControl(),
 
+    }),
+    this.saveNameForm = new FormGroup({
+      femaleNameSave: new FormControl(),
+      maleNameSave: new FormControl(),
+
     })
   }
 
   getNames(){
+
 
   this.maleName = document.getElementById("maleNameChoiceID") as HTMLInputElement;
   this.maleNameChoice = this.maleName.checked;
@@ -48,15 +59,29 @@ export class NameGenComponent implements OnInit {
 
     });
 
+    this.testName = JSON.stringify(this.generatedMaleNames);
 
 
+    this.namesArray = this.testName.split(',');
+    let re = /",/gi;
+    let reTwo = /"/gi;
 
+    this.newStr = this.namesArray.toString().replace(re,"\n");
+    this.newStr2 = this.newStr.toString().replace(reTwo,"").replace('[',"").replace(']',"").split('\n');
   }
   else if(this.maleNameChoice == false && this.femaleNameChoice){
     this.service.getFemaleNamesAmount(5).subscribe(names => {
       this.generatedFemaleNames = names;
     });
+    this.testName = JSON.stringify(this.generatedFemaleNames);
 
+
+    this.namesArray = this.testName.split(',');
+    let re = /",/gi;
+    let reTwo = /"/gi;
+
+    this.newStr = this.namesArray.toString().replace(re,"\n");
+    this.newStr2 = this.newStr.toString().replace(reTwo,"").replace('[',"").replace(']',"").split('\n');
 
   }
   else{
