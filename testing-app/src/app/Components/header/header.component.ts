efc6e69ff2a5c586df14de4dcd842ae402b11668
Emil Boye <emib@pc-tec.local>
2021-06-04 07:42:05 +0200

    Updated header - Emil

--- testing-app/src/app/Components/header/header.component.ts
+++ testing-app/src/app/Components/header/header.component.ts
@@ -12,14 +12,17 @@ import { Users } from 'src/app/model/Users';
 })
 export class HeaderComponent implements OnInit {
   users: Users[];
+  confirmPass: any;
   @Input() user: Users;
+  userObj: Users = {userID: 0,userEmail: "", passwordHash: ""}
+  userChecked: boolean = true;
 
   public loginError:String;
   constructor(private http:HttpClient, private service:HttpService) { }
 
   createUserForm = new FormGroup(
     {
-      userEmail: new FormControl(''),
+      userEmail: new FormControl('', [Validators.required, Validators.email]),
       passwordHash: new FormControl(''),
     }
   );
@@ -44,26 +47,72 @@ export class HeaderComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   onSubmitCreate(){
+
+    this.confirmPass = document.getElementById("confirmAccPass") as HTMLInputElement;
+
     console.log(this.createUserForm.value);
 
     // tslint:disable-next-line: deprecation
     // tslint:disable-next-line: deprecation
+    if (this.confirmPass.value == this.createUserForm.value.passwordHash){
+      console.log('Test');
 
-    if (this.createUserForm.value.passwordHash.length > 8 && this.createUserForm.value.userEmail){
+<<<<<<< HEAD
+      if (this.createUserForm.value.passwordHash.length >= 8 && this.createUserForm.value.userEmail){
       alert("User created");
 
       this.service.postUser(this.createUserForm.value).subscribe(user => console.log(user));
-    }
-    else {
-      alert("Password needs to be atleast 8 characters long");
+      }
+      else{
+        if(this.createUserForm.value.userEmail.length < 1){
+          alert("You need your Email");
+        }
+        else{
+          alert("Password needs to be atleast 8 characters long");
+        }
+      }
 
+=======
+    if (this.createUserForm.value.passwordHash.length > 8 && this.createUserForm.value.userEmail != null){
+
+      this.service.postUser(this.createUserForm.value).subscribe(user => console.log(user));
+      alert("User created");
+>>>>>>> 41308b0225e3bdb66cbb7b74399d118a697adb6a
     }
+
+    else{
+      alert("Password must be the same!")
+    }
+
   }
   // tslint:disable-next-line: typedef
   onSubmitLogin(){
     // tslint:disable-next-line: deprecation
-    this.service.getUser().subscribe(user => console.log(user));
 
+
+    this.userObj.userEmail = this.loginUserForm.value.userEmail;
+    this.userObj.passwordHash = this.loginUserForm.value.passwordHash;
+
+    var user = this.users.filter(u => u.userEmail == this.loginUserForm.value.userEmail && u.passwordHash == this.loginUserForm.value.passwordHash);
+
+    if(user.length == 0){
+      alert("Wrong email or password");
+      this.userChecked = false;
+    }
+    else if(user.length == 1){
+      this.userChecked = true;
+      this.service.ValidateUser(this.userObj).subscribe();
+      this.loginUserForm.reset();
+      localStorage.setItem('User','Logged In');
+      alert("You are now logged in!");
+      this.userChecked = false;
+    }
+  }
+  onSubmitLogout(){
+    if(localStorage.getItem('User')){
+      window.localStorage.clear();
+      window.location.reload();
+    }
   }
 
 }
