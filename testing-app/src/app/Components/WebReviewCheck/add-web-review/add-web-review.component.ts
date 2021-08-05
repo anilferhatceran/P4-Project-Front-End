ca90d3ff3fd5e98a6c96d5e61eeb7127a6675dd9
EmirhanYilmaz <48058646+EmirhanYilmaz@users.noreply.github.com>
2021-08-05 08:29:51 +0200

    generated new component named add-web-review

---
+++ testing-app/src/app/Components/WebReviewCheck/add-web-review/add-web-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-web-review',
+  templateUrl: './add-web-review.component.html',
+  styleUrls: ['./add-web-review.component.css']
+})
+export class AddWebReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
